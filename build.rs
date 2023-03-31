15435634ec03c72449992dc24a659a1316686810
Martin Zeller <mz.bremerhaven@gmail.com>
2023-03-31 17:23:21 +0200

    Use phf_codegen to generade phf map in build.rs

--- build.rs
+++ build.rs
@@ -1,26 +1,34 @@
-use std::{env, fmt::Write, fs, path::Path};
+use std::{
+    env,
+    fs::{self, File},
+    io::BufWriter,
+    io::Write,
+    path::Path,
+};
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("stationpedia.rs");
 
-    let mut output =
-        "pub(crate) const HASH_NAME_LOOKUP: phf::Map<&'static str, &'static str> = phf_map! {\n"
-            .to_string();
+    let mut map_builder = ::phf_codegen::Map::new();
 
     let infile = Path::new("stationpedia.txt");
     let contents = fs::read_to_string(infile).unwrap();
 
     for line in contents.lines() {
-        let (hash, name) = {
-            let mut it = line.splitn(2, ' ');
-            (it.next().unwrap(), it.next().unwrap())
-        };
-
-        writeln!(&mut output, "\"{hash}\" => \"{name}\",").unwrap();
+        let mut it = line.splitn(2, ' ');
+        map_builder.entry(it.next().unwrap(), &format!("\"{}\"", it.next().unwrap()));
     }
-    writeln!(&mut output, "}};").unwrap();
 
-    fs::write(dest_path, output).unwrap();
+    let output_file = File::create(dest_path).unwrap();
+    let mut writer = BufWriter::new(&output_file);
+
+    write!(
+        &mut writer,
+        "pub(crate) const HASH_NAME_LOOKUP: phf::Map<&'static str, &'static str> = {};\n",
+        map_builder.build()
+    )
+    .unwrap();
+
     println!("cargo:rerun-if-changed=stationpedia.txt");
 }
